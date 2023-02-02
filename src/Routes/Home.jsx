01edd9e5e0e5c4977d551d6f67ceec1041495e41
carren <chepkorircarren9@gmail.com>
2023-02-02 13:48:32 +0300

    imported Herosection component

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HeroSection from "../Components/HeroSection";
 import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 };
