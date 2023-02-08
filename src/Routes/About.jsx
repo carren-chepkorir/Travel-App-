00e7dce69202bfe1ac0b070c2fd42f1edc9de834
carren <chepkorircarren9@gmail.com>
2023-02-08 10:53:50 +0300

    created a array of items to be mapped

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Slider from "../Components/Slider";
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <Slider />
     </div>
   );
 };
