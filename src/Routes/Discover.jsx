87aeb3c4d467c6d28aaa5e8121d8464555dca06e
carren <chepkorircarren9@gmail.com>
2023-02-09 15:19:30 +0300

    imported the explore component

--- src/Routes/Discover.jsx
+++ src/Routes/Discover.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Explore from "../Components/Explore";
 import Navbar from "../Components/Navbar";
 
 const Discover = () => {
   return (
     <div>
       <Navbar />
+      <Explore />
     </div>
   );
 };
