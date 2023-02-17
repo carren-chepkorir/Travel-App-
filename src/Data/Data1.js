b891fabbe1a6abbdd030f98ad27048402490b761
carren <chepkorircarren9@gmail.com>
2023-02-17 14:59:34 +0300

    modified json data

---
+++ src/Data/Data1.js
@@ -0,0 +1,114 @@
+const Data1 = [
+  {
+    id: 1,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/139611386_3974096652647871_2509093287444516970_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=oWKnFD9KjB8AX_jdDBM&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDz9HJqC1jbOrzQJOxqyCtfSXJYHpXSHNxZB7OUqjB2uA&oe=64158778",
+    title: "Maasai Mara ",
+    Message:
+      "Reservation process was top ,fast response from Nicholas, clear advice and appointments for meeting with our guide and payment perfect",
+  },
+  {
+    id: 2,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/245128009_4772717266119135_5129336026203964242_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=fnehJQOh52YAX-zZW59&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfCVssnAgv8CMNX7ql1iVvrKe4WqeH7ZZpCEVYs4oPtSDA&oe=63F25289",
+    title: "Second Slide",
+    Message:
+      "Reservation process was top ,fast response from Nicholas, clear advice and appointments for meeting with our guide and payment perfect.",
+  },
+  {
+    id: 3,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/245424927_4772715759452619_6299850450066227654_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=YVSqoa88v9EAX_xD3yj&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDppIYBOBInYpYGlQGcL5J_6-Oy-bI7vcc_0vtesDu_Lg&oe=63F3B522",
+    title: "Third Slide",
+    Message:
+      "We had a great time exploring Amboseli and Tsavo West with our guide Nicholas. He has more than 20 years of experience and it definitely helped to make our safari great",
+  },
+  {
+    id: 4,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/175178549_4238316042892596_8655164066814100400_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Jk7XRMzVy-MAX-wDFbX&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDFnlPRTi7vJowbqARP3mr52njEUXFNkGdKVOmwSjDpew&oe=641576A5",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 5,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/241157690_4642989322425264_8523821602183927752_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=ErO2psRAmRIAX_cAbOF&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDl1rAC6Es7umSaYi63lW2EhQDfyUwfIpBhipvbqLhmUw&oe=63F3D8B0",
+    title: "Fourth Slide",
+    Message: " with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 6,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/241194248_4646317235425806_1457522090931244979_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=szpY-AcaP54AX-3uAV_&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDoXg9GNvB_1cFsbdhz_kuFIo86Lhj5q4CyltnKqlGbSQ&oe=63F35ADB",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 7,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/241074877_4641375329253330_7538514904153022145_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=e27BJSzviHIAX-KRUe2&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfCz_7scpvzeVFrU6F2sbvppn1sB5gV1yKq-ECc8AxSeWg&oe=63F2CD06",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 8,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/241093702_4642989019091961_4656909960302366099_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=MCUsBfr-DhIAX_8zD_F&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfDWDlpdCwXzpxdj5S9ucUpx_0ob4EkRFzcmCHqjJFZ0xw&oe=63F277AE",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 9,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/169123629_4468320409892157_3598336307896449805_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=LBF4RZYeehgAX-N82A6&_nc_oc=AQmJ4pw537mbmrzqYsCUG5Zc6aSl5k__DBWXdv1el0wFTDwtGm6tHZYVAsZAIU-F_Fc&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfBVi6f6AjFTJY9OaPga-isydA1TQ0jdAXry94KqOgB-UQ&oe=63F38C3B",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 10,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/207408099_4442496169141248_5526695983088564176_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=oRGqPktAuEIAX_tLxXF&tn=-NX_OfLJqFZT7gcP&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfAGcNknbMoGbkLrUdwJwqrU918Dj92lAJRUTlkEtgxNjQ&oe=64156A82",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 11,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/198389973_4387992297924969_918278452671075422_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=BewRNHQO2i8AX-qgS1O&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfBvhNiy0MlEMZxDUuU5fnQl3v76ilS_CbPZjKqpImDwww&oe=64156844",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 12,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/197673642_4387765651280967_1373988994299795473_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Giy310gkfxgAX-W07Z2&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfAQ9aDsj9TxnvmJdE4xjfdrMQTvIh3KGGvohXfjqG6Ckw&oe=64156C42",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 13,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/188027794_4341271209263745_8064756587377469832_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=RkhlBJhsbTMAX_yxSsL&tn=-NX_OfLJqFZT7gcP&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfASdVlszyWZtnaLWkG8d_ZrIovEPFkaHS977LWMRqAq6w&oe=64157007",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+  {
+    id: 14,
+    image:
+      "https://z-p3-scontent-mba1-1.xx.fbcdn.net/v/t1.6435-9/188972407_4341262489264617_8962558249542929314_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=R_BgY2DQt9AAX9x31-_&_nc_oc=AQkzKQbTOtHW0kvtYw4FFFrKXZ2ORmN4KMtS2azY_64K478DmZ3zlVj6ICRaYI3Lo_k&_nc_ht=z-p3-scontent-mba1-1.xx&oh=00_AfBy8iZ7-uwR31azZEg-ac580vU_bKCY7MAlm4BLqvFA5g&oe=6415581C",
+    title: "Fourth Slide",
+    Message:
+      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+  },
+];
+export default Data1;
