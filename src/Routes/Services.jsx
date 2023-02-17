59f5bd13acdfa3bb662b243f67f1da7436ecb2aa
carren <chepkorircarren9@gmail.com>
2023-02-17 15:00:25 +0300

    imported service page

--- src/Routes/Services.jsx
+++ src/Routes/Services.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import MyServices from "../Components/MyServices";
 import Navbar from "../Components/Navbar";
 
 const Services = () => {
   return (
     <div>
       <Navbar />
+      <MyServices />
     </div>
   );
 };
