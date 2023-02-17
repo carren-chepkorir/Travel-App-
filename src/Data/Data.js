77642b18aff2e0133743e9948427a2fbc141cb5b
carren <chepkorircarren9@gmail.com>
2023-02-17 14:59:16 +0300

    Modified json data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -5,7 +5,7 @@ const Data = [
       "https://images.unsplash.com/photo-1569865223473-40b24125ed40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNhZmFyaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     title: "First Slide",
     Message:
-      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+      "Reservation process was top ,fast response from Nicholas, clear advice and appointments for meeting with our guide and payment perfect",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const Data = [
       "https://images.unsplash.com/photo-1610624393235-c1b89453b7d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FmYXJpc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     title: "Second Slide",
     Message:
-      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+      "Reservation process was top ,fast response from Nicholas, clear advice and appointments for meeting with our guide and payment perfect.",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const Data = [
       "https://images.unsplash.com/photo-1636113343724-cf58056f21de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FmYXJpc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     title: "Third Slide",
     Message:
-      "Travelling with Kip is more fantastic,I enjoyed a trip to Maasai Mara",
+      "We had a great time exploring Amboseli and Tsavo West with our guide Nicholas. He has more than 20 years of experience and it definitely helped to make our safari great",
   },
   {
     id: 4,
